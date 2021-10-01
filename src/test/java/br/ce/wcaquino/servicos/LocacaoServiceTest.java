4d0dd59169df7da117b34402e24514150fae0f2c
Gustavo <47989383+Gustavocdm@users.noreply.github.com>
2021-09-30 21:02:47 -0300

    Exceção esperada no teste - Forma elegante

--- src/test/java/br/ce/wcaquino/servicos/LocacaoServiceTest.java
+++ src/test/java/br/ce/wcaquino/servicos/LocacaoServiceTest.java
@@ -25,7 +25,7 @@ public class LocacaoServiceTest {
 		//cenario
 		LocacaoService service = new LocacaoService();
 		Usuario usuario = new Usuario("Usuario 1");
-		Filme filme = new Filme("Filme 1", 0, 5.0);
+		Filme filme = new Filme("Filme 1", 2, 5.0);
 		
 		//acao
 		Locacao locacao = service.alugarFilme(usuario, filme);
@@ -34,5 +34,16 @@ public class LocacaoServiceTest {
 		error.checkThat(locacao.getValor(), is(equalTo(5.0)));
 		error.checkThat(isMesmaData(locacao.getDataLocacao(), new Date()), is(true));
 		error.checkThat(isMesmaData(locacao.getDataRetorno(), obterDataComDiferencaDias(1)), is(true));
+	}
+	
+	@Test(expected=Exception.class)
+	public void testLocacao_filmeSemEstoque() throws Exception {
+		//cenario
+		LocacaoService service = new LocacaoService();
+		Usuario usuario = new Usuario("Usuario 1");
+		Filme filme = new Filme("Filme 1", 0, 5.0);
+		
+		//acao
+		service.alugarFilme(usuario, filme);
 	}
 }
