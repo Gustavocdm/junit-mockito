f6380234e32d758f1801a311eb96afd583df79a0
Gustavo <47989383+Gustavocdm@users.noreply.github.com>
2021-10-01 22:47:52 -0300

    Adicionando ordem alfabética aos testes

--- src/test/java/br/ce/wcaquino/servicos/OrdemTest.java
+++ src/test/java/br/ce/wcaquino/servicos/OrdemTest.java
@@ -1,26 +1,23 @@
 package br.ce.wcaquino.servicos;
 
 import org.junit.Assert;
+import org.junit.FixMethodOrder;
 import org.junit.Test;
+import org.junit.runners.MethodSorters;
 
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class OrdemTest {
 	
 	public static int contador = 0;
 	
-//	@Test
+	@Test
 	public void inicia() {
 		contador = 1;
 	}
 	
-//	@Test
+	@Test
 	public void verifica() {
 		Assert.assertEquals(1, contador);
-	}
-	
-	@Test
-	public void testeGeral() {
-		inicia();
-		verifica();
 	}
 	
 }
