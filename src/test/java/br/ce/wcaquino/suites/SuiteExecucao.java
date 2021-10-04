f4187eeda19223863294b022b79c44955151639f
Gustavo <47989383+Gustavocdm@users.noreply.github.com>
2021-10-03 23:11:03 -0300

    Suíte de teste

---
+++ src/test/java/br/ce/wcaquino/suites/SuiteExecucao.java
@@ -0,0 +1,30 @@
+package br.ce.wcaquino.suites;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+import br.ce.wcaquino.servicos.CalculadoraTest;
+import br.ce.wcaquino.servicos.CalculoValorLocacaoTest;
+import br.ce.wcaquino.servicos.LocacaoServiceTest;
+
+// @RunWith(Suite.class)
+@SuiteClasses({
+	CalculadoraTest.class,
+	CalculoValorLocacaoTest.class,
+	LocacaoServiceTest.class
+})
+public class SuiteExecucao {
+	
+	@BeforeClass
+	public static void before() {
+		// Pode ser utilizado o @BeforeClass em uma suíte de teste 
+	}
+	
+	@AfterClass
+	public static void after() {
+		// Pode ser utilizado o @AfterClass em uma suíte de teste
+	}
+}
